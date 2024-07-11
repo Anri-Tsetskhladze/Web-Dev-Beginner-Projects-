16d98ba7837185c395eb137956724b32ad83380f
Anri Tsetskhladze <tsetskhladzeanri209@gmail.com>
2024-07-12 00:32:55 +0400

    Add files via upload

---
+++ Frontend Dev Site/script.js
@@ -0,0 +1,23 @@
+document.querySelector('.confirm-button').addEventListener('click', () => {
+    alert('Confirm button clicked!');
+});
+
+document.querySelector('.alert-button').addEventListener('click', () => {
+    alert('Alert button clicked!');
+});
+
+document.querySelector('.error-button').addEventListener('click', () => {
+    alert('Error button clicked!');
+});
+
+document.querySelector('.warning-button').addEventListener('click', () => {
+    alert('Warning button clicked!');
+});
+
+document.querySelector('.info-button').addEventListener('click', () => {
+    alert('Info button clicked!');
+});
+
+document.querySelector('.secondary-button').addEventListener('click', () => {
+    alert('Secondary button clicked!');
+});
